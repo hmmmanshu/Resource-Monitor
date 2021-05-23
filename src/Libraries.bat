e9514932b7d7f37675db1c1310d54e30732da058
Himanshu Negi <51152254+Bot-7037@users.noreply.github.com>
2021-05-23 16:55:23 +0530

    Add files via upload

---
+++ src/Libraries.bat
@@ -0,0 +1,5 @@
+pip install psutil
+pip install pandas
+pip install win10toast
+pip install termcolor
+pip install notify2
---
+++ src/Libraries.sh
@@ -0,0 +1,5 @@
+sudo pip install psutil
+sudo pip install pandas
+sudo pip install win10toast
+sudo pip install notify2
+sudo pip install termcolor
